149726a01e2b3fe5d9300c9355e275458527ce17
Jeff Scott Brown <brownj@ociweb.com>
2016-01-15 19:06:00 -0600

    create Person instances at startup

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,10 @@
 class BootStrap {
 
     def init = { servletContext ->
+    // uncomment to create Person objects on startup...
+//        40.times {
+//            new demo.Person(name: "Name $it", isActive: true).save()
+//        }
     }
     def destroy = {
     }
